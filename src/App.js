9528c868939128213204c23ebab4c72472b9d58d
Victor <amavictor47@gmail.com>
2022-05-17 13:17:26 +0100

    first section. Arranging everything into their components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from "./component/directory/directory";
 
 function App() {
+  const category =
+      [
+        {
+          "id": 1,
+          "title": "hats",
+          "imageUrl": "https://images.unsplash.com/photo-1529958030586-3aae4ca485ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+        },
+        {
+          "id": 2,
+          "title": "jackets",
+          "imageUrl": "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80"
+        },
+        {
+          "id": 3,
+          "title": "sneakers",
+          "imageUrl": "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+        },
+        {
+          "id": 4,
+          "title": "womens",
+          "imageUrl": "https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d29tZW5zJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        },
+        {
+          "id": 5,
+          "title": "mens",
+          "imageUrl": "https://images.unsplash.com/photo-1618886614638-80e3c103d31a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=370&q=80"
+        }
+      ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Directory categories = {category}/>
   );
 }
 
---
+++ src/component/directory/directory.jsx
@@ -0,0 +1,14 @@
+import CategoryItemComponent from "../category-item/category-item.component";
+import './directory.styles.scss'
+
+
+export default function Directory({categories}){
+    return(
+        <div className="directory-container">
+            {categories.map((category)=>(
+                <CategoryItemComponent key={category.id} category={category}/>
+            ))}
+
+        </div>
+    )
+}
